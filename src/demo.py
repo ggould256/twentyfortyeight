6f43748cd104a7e89abe984acb814ffd7d4c2fcf
Grant Gould <ggould@jaybridge.com>
2014-03-21 10:51:23 -0400

    Add gloating to strategy and demo

--- src/demo.py
+++ src/demo.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
 from rules import Game
-from strategy import RandomStrategy
+import strategy
 
 if __name__ == '__main__':
     game = Game()
-    strategy = RandomStrategy()
+    strategy = strategy.SpinnyStrategy()
     running = True
     while running:
         turn_outcome = game.do_turn(
             strategy.get_move(game._board, game._score))
         running = (turn_outcome != Game.GAMEOVER)
+    strategy.notify_outcome(game._board, game._score)
     print game._score
--- src/strategy.py
+++ src/strategy.py
@@ -6,12 +6,22 @@ from rules import Game
 
 class Strategy(object):
     """An abstract class for 2048 strategies.  Implementors will wish
-    to override get_move()."""
+    to override get_move().  A strategy will be instantiated once at
+    the beginning of each games, and disposed of once the game is
+    complete."""
 
     def get_move(self, board, score):
+        """
+        Given the current state of the game, return the move chosen by
+        this strategy.  Subclasses must implement this method.
+        """
         raise NotImplementedError(
             "Strategy subclasses must implement get_move().")
 
+    def notify_outcome(self, board, score):
+        """Optionally, subclasses may choose to be notified of the
+        outcome of the game.  This is your opportunity to gloat."""
+        pass
 
 class RandomStrategy(Strategy):
     def __init__(self):
@@ -28,3 +38,10 @@ class SpinnyStrategy(Strategy):
     def get_move(self, board, score):
         self._counter += 1
         return Game.DIRECTIONS[self._counter % len(Game.DIRECTIONS)]
+
+    def notify_outcome(self, board, score):
+        best_tile = max(max(column) for column in board)
+        if best_tile >= 2048:
+            print "Oh yeah, I rock, I got a", best_tile
+        else:
+            print "Oops, I only got", best_tile
