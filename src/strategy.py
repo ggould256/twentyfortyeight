6ae5aa7d8336b9fa37d5077191caea624467cb4d
Grant Gould <ggould@alum.mit.edu>
2014-03-23 13:57:09 -0400

    Factor class naming into strategy and default it via reflection.

--- src/strategy.py
+++ src/strategy.py
@@ -7,9 +7,14 @@ from rules import Game
 
 class Strategy(object):
     """An abstract class for 2048 strategies.  Implementors will wish
-    to override get_move().  A strategy will be instantiated once at
-    the beginning of each games, and disposed of once the game is
+    to override at least get_move().  A strategy will be instantiated
+    once at the beginning of each games, and disposed of once the game is
     complete."""
+
+    def name(self):
+        """@return a name that can be used in printing messages about this
+        strategy"""
+        return self.__class__.__name__
 
     def get_move(self, board, score):
         """
--- src/strategy_evaluator.py
+++ src/strategy_evaluator.py
@@ -9,18 +9,17 @@ from rules import Game
 class StrategyEvaluator(object):
     NUM_RUNS = 100
 
-    def __init__(self, strategy_class):
-        self._strategy_class = strategy_class
+    def __init__(self, strategy):
+        self._strategy = strategy
 
     def one_run(self):
         game = Game()
-        strategy = self._strategy_class()
         running = True
         while running:
             turn_outcome = game.do_turn(
-                strategy.get_move(game.board(), game.score()))
+                self._strategy.get_move(game.board(), game.score()))
             running = (turn_outcome != Game.GAMEOVER)
-        strategy.notify_outcome(game.board(), game.score())
+        self._strategy.notify_outcome(game.board(), game.score())
         return game.score()
 
     def evaluate(self):
@@ -32,9 +31,8 @@ class StrategyEvaluator(object):
 
 if __name__ == '__main__':
     import strategy
-    e1 = StrategyEvaluator(strategy.RandomStrategy)
-    e1_score = e1.evaluate()
-    print "RandomStrategy", e1_score
-    e2 = StrategyEvaluator(strategy.SpinnyStrategy)
-    e2_score = e2.evaluate()
-    print "SpinnyStrategy", e2_score
+    for strat in [strategy.RandomStrategy(),
+                  strategy.SpinnyStrategy()]:
+        evaluator = StrategyEvaluator(strat)
+        score = evaluator.evaluate()
+        print strat.name(), score
